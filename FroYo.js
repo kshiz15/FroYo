0b452e6dcbadae25e2138ac20cdf1e69f90d3c30
Kristen Connors <kdconnors15@gmail.com>
2024-12-17 09:35:41 -0700

    finished for of loop

--- FroYo.js
+++ FroYo.js
@@ -1,17 +1,27 @@
+///create prompt asking user to enter froyo flavors separated by comma
+//enter ice cream flavors
+
 const userInput = prompt(
   "Please enter FrYo flavors separated by a comma",
   "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
 
+//turn array into object
+
 const flavorArray = userInput.split(",");
 console.log(flavorArray);
 
-const froYoFlavors = {};
+const count = {};
 
 for (const key of flavorArray) {
-  froYoFlavors[key] = 0;
+  flavorArray[key] = 0;
+  if (count[key]) {
+    count[key] += 1;
+  } else {
+    count[key] = 1;
+  }
 }
-console.log(froYoFlavors);
+console.log(count);
 
 //create a loop of flavors
 //if array's value is = to key, add 1 to key's value
